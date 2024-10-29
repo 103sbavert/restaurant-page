55272c41f3892858180ae02a731c09a558e1a913
Yusuf Jamal <yusufjamal1372@gmail.com>
2024-10-29 17:51:20 +0530

    Half-finish navigation item behavior, clean the UI

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 import "./styles.css"
+
+let selectedNavItemIndex = 0
+let navItemButtons = document.querySelectorAll(".nav-list > .nav-item > .nav-button")
+
+function selectButton(event) {
+    let list = event.target.closest(".nav-list").children
+    let selected = event.target.closest(".nav-item")
+    let currentSelectedIndex = Array.from(list).indexOf(selected)
+    let prevSelected = list[selectedNavItemIndex]
+
+    prevSelected.classList.remove("selected")
+    selected.classList.add("selected")
+
+    selectedNavItemIndex = currentSelectedIndex
+}
+
+for (let i = 0; i < navItemButtons.length; i++) {
+    let button = navItemButtons[i]
+    
+    button.addEventListener("click", selectButton)
+
+    console.log(button)
+}
